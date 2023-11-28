d4bbd71a2ab482e8a51aee33898b5672e77cfbb7
aLEn203 <aLEn.studios2@gmail.com>
2023-11-28 00:14:59 -0500

    Still Working

--- Assets/Scripts/TiltController_Revised.cs
+++ Assets/Scripts/TiltController_Revised.cs
@@ -9,6 +9,7 @@ public class TiltController_Revised : MonoBehaviour
     //Ship Tilt variables
     //private float tiltTimer;
     public float playerTiltForce = 0.5f;
+    private float crewTiltForce = 0.5f;
     //public float tiltDirection;
 
     //Sin Wave Variables
@@ -16,16 +17,17 @@ public class TiltController_Revised : MonoBehaviour
     public float magnitude = 5f;
 
     //RigidBodies Variables
-    private Rigidbody2D rb;
+    //private Rigidbody2D rb;
     private Rigidbody2D player;
     private GameObject[] roles;
     private GameObject[] crew;
 
     //Positional Variables
-    Vector3 shipMovePos;
     Vector3 shipStartPos;
     Vector3[] role_initial_positions;
+    //Vector3[] role_movement_positions;
     Vector3[] crew_initial_positions;
+    //Vector3[] crew_movement_positions;
 
     // Start is called before the first frame update
     void Start()
@@ -39,15 +41,21 @@ public class TiltController_Revised : MonoBehaviour
         //rb = GetComponent<Rigidbody2D>(); //Ship itself
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
         //Gameobjects
-        //roles = GameObject.FindGameObjectsWithTag("Role");
-        //crew = GameObject.FindGameObjectsWithTag("Crew");
+        roles = GameObject.FindGameObjectsWithTag("Role");
+        crew = GameObject.FindGameObjectsWithTag("Crew");
 
         //Initiliaze Starting Position
         shipStartPos = transform.position;
+        
         role_initial_positions = new Vector3[roles.Length];
         role_initial_positions = setInitialPositions(roles, role_initial_positions);
+        //role_movement_positions = new Vector3[roles.Length];
+        //role_movement_positions = setInitialPositions(roles, role_movement_positions);
+
         crew_initial_positions = new Vector3[crew.Length];
         crew_initial_positions = setInitialPositions(crew, crew_initial_positions);
+        //crew_movement_positions = new Vector3[crew.Length];
+        //crew_movement_positions = setInitialPositions(crew, crew_movement_positions);
     }
 
     // Update is called once per frame
@@ -60,23 +68,20 @@ public class TiltController_Revised : MonoBehaviour
         addForceForGameObjects(roles, frequency, magnitude);*/
 
         //Ship Tilt
+        //Debug.Log(shipStartPos * Mathf.Sin(Time.time * frequency) * magnitude);
         transform.position += shipStartPos * Mathf.Sin(Time.time * frequency) * magnitude;
 
         //Player Tilt
 
         //Crew Tilt
+        crew = GameObject.FindGameObjectsWithTag("Crew");
+        addTiltToGameObjects(crew, crew_initial_positions);
 
         //Role Tilt
+        addTiltToGameObjects(roles, role_initial_positions);
     }
     
-    public void addForceForGameObjects(GameObject[] array, float frequency, float magnitude)
-    {
-        for (int i = 0; i < array.Length; i++)
-        {
-            Rigidbody2D item = array[i].GetComponent<Rigidbody2D>();
-            item.AddForce(new Vector2(0f, item.transform.position.y) * Mathf.Sin(Time.time * frequency) * magnitude);
-        }
-    }
+
     private Vector3[] setInitialPositions(GameObject[] roles, Vector3[] role_initial_positions)
     {
         for (int i = 0; i < roles.Length; i++)
@@ -97,4 +102,24 @@ public class TiltController_Revised : MonoBehaviour
             //crew[i].transform.position = new Vector3(crew_initial_positions[i].x, crew_initial_positions[i].y + (0.15f * moveSpeed), crew_initial_positions[i].z);
         }
     }
+
+    public void addForceForGameObjects(GameObject[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            Rigidbody2D item = array[i].GetComponent<Rigidbody2D>();
+            item.AddForce(new Vector2(0f, item.transform.position.y) * Mathf.Sin(Time.time * frequency) * magnitude * playerTiltForce);
+        }
+    }
+
+    public void addTiltToGameObjects(GameObject[] array, Vector3[] startingPos)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            Vector3 movement = startingPos[i];
+            movement = startingPos[i] * Mathf.Sin(Time.time * frequency) * magnitude;
+            array[i].transform.position = new Vector3(startingPos[i].x, startingPos[i].y + movement.y, startingPos[i].z);
+        }
+    }
+
 }
