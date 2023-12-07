118103c597683b1b1af0e3bda1bd8c93235638c0
Elis Joynes <smj.joynes@gmail.com>
2023-12-07 17:04:42 -0500

    Dani tutorial file

--- Assets/Scripts/tutorial_controller.cs
+++ Assets/Scripts/tutorial_controller.cs
@@ -4,142 +4,54 @@ using UnityEngine;
 
 public class tutorial_controller : MonoBehaviour
 {
-    public GameObject[] popUps;
-    private int popUpIndex = 0;
+    public GameObject[] popUps;  // Assign your popups in the inspector
+    private int popUpIndex = 1;
+    private bool waitForSpacebar = true;
 
-    public float waitTime = 5f;
-
-    public int didWalk = 0;
-   
     void Start()
     {
-        StartCoroutine(PopUps());
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if (didWalk == 0)
-        //{
-        //    if (Input.GetKey(KeyCode.F))
-        //    {
-        //        Debug.Log("wasd accepted");
-        //        didWalk = 1;
-        //        popUpIndex++;
-        //        Debug.Log("Walked, moving on" + popUpIndex);
+        // Check for spacebar input
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Trigger the next popup
+            ShowNextPopup();
+        }
 
-        //    }
-        //}
 
-        //Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D)
 
 
     }
 
-    public IEnumerator PopUps()
+    void ShowNextPopup()
     {
-        Debug.Log("Popsups started");
-
-        if (popUpIndex == 0)
+        if (popUpIndex < popUps.Length)
         {
+            // Disable the previous popup
+            if (popUpIndex > 0)
+            {
+                popUps[popUpIndex - 1].SetActive(false);
+            }
+
+            // Enable the current popup
             popUps[popUpIndex].SetActive(true);
-            yield return new WaitForSeconds(5f);
-            popUps[popUpIndex].SetActive(false);
+
+            // Increment index for the next popup
             popUpIndex++;
             Debug.Log("Next popup");
         }
-        if (popUpIndex == 1)
+        else
         {
-            popUps[popUpIndex].SetActive(true);
-            yield return new WaitForSeconds(5f);
-            popUps[popUpIndex].SetActive(false);
-            popUpIndex++;
-            Debug.Log("Next popup");
-        }
-        if (popUpIndex == 2)
-        {
-            popUps[popUpIndex].SetActive(true);
-            yield return new WaitForSeconds(5f);
-            popUps[popUpIndex].SetActive(false);
-            popUpIndex++;
-            Debug.Log("Next popup");
-
-        }
-        if (popUpIndex == 3)
-        {
-            popUps[popUpIndex].SetActive(true);
-            yield return new WaitForSeconds(5f);
-            popUps[popUpIndex].SetActive(false);
-            popUpIndex++;
-            Debug.Log("Next popup");
-        }
-        if (popUpIndex == 4)
-        {
-            popUps[popUpIndex].SetActive(true);
-            yield return new WaitForSeconds(5f);
-            popUps[popUpIndex].SetActive(false);
-            popUpIndex++;
-            Debug.Log("Next popup");
-        }
-        if (popUpIndex == 5)
-        {
-            popUps[popUpIndex].SetActive(true);
-            yield return new WaitForSeconds(5f);
-            popUps[popUpIndex].SetActive(false);
-            popUpIndex++;
-            Debug.Log("Next popup");
+            Debug.Log("No more popups");
+            popUpIndex = 0;
+            // If there are no more popups, you can perform any other actions here
         }
 
-        if (popUpIndex == 6)
-        {
-            popUps[popUpIndex].SetActive(true);
-            yield return new WaitForSeconds(5f);
-            popUps[popUpIndex].SetActive(false);
-            popUpIndex++;
-            Debug.Log("Next popup");
-        }
-
-        if (popUpIndex == 7)
-        {
-            popUps[popUpIndex].SetActive(true);
-            yield return new WaitForSeconds(5f);
-            popUps[popUpIndex].SetActive(false);
-            popUpIndex++;
-            Debug.Log("Next popup");
-        }
-
-        if (popUpIndex == 8)
-        {
-            popUps[popUpIndex].SetActive(true);
-            yield return new WaitForSeconds(5f);
-            popUps[popUpIndex].SetActive(false);
-            popUpIndex++;
-            Debug.Log("Next popup");
-        }
-
-        if (popUpIndex == 9)
-        {
-            popUps[popUpIndex].SetActive(true);
-            yield return new WaitForSeconds(5f);
-            popUps[popUpIndex].SetActive(false);
-            popUpIndex++;
-            Debug.Log("Next popup");
-        }
-
-        if (popUpIndex == 10)
-        {
-            popUps[popUpIndex].SetActive(true);
-            yield return new WaitForSeconds(5f);
-
-            Debug.Log("Next popup");
-        }
-
-
-
-
-
-
-
-        //~~~~~~~~~
     }
+
 }
